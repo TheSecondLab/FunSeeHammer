a78c24f37a1c115856575aadea3425313bb99c8b
MeatPieYan <robinyzg@hotmail.com>
2018-02-09 15:56:41 +0800

    0.0,1

---
+++ index.js
@@ -0,0 +1,9 @@
+const babylon = require('babylon');
+const fs = require('fs');
+const path = require('path');
+
+const testCode = fs.readFileSync(path.resolve(__dirname, '../FunSee/index.js')).toString();
+console.log(testCode);
+const ast = babylon.parse(testCode);
+debugger
+console.log(ast);
