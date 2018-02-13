098e2e06535ccf284dfc52050924a20342e88115
MeatPieYan <robinyzg@hotmail.com>
2018-02-13 17:53:04 +0800

    prototype version

---
+++ bin/funsee-hammer.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const { version } = require('../package.json');
+
+const { argv } = yargs;
+
+yargs
+  .version(version)
+  .command('dev [filename]', 'run the server', (yargs) => {
+    yargs
+      .positional('filename', {
+        describe: 'dev: startup file',
+        type: 'string',
+      })
+      .version(false);
+  }, (argv) => {
+    require('../lib/startUp/dev')(argv);
+  }).argv
