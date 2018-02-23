44ce7a576f7b006e1e69efe58b145d0864a5f806
MeatPieYan <robinyzg@hotmail.com>
2018-02-23 15:20:10 +0800

    for dev server

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: '../../FunSeeBoilerplate/server/server',
-  output: {
-    path: path.resolve(__dirname,'../dist'),
-    filename: 'bundle.js'
-  },
-  target: 'node'
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }
+    }]
+  }
 };
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
+
+const webpackBase = require('./webpack.base');
+
+// for test
+global.__ROOT_PATH__ = global.__ROOT_PATH__ || '/Users/robin/Documents/project/FS/FunSeeBoilerplate';
+global.__FS_PATH__ = global.__FS_PATH__ || '/Users/robin/Documents/project/FS/FunSee'
+
+module.exports = merge(webpackBase, {
+  devtool: 'source-map',
+  entry: [
+    path.relative(global.__ROOT_PATH__, `${global.__FS_PATH__}/lib/client/index.js`)
+  ],
+  output: {
+    filename: 'test.js',
+    path: path.resolve(global.__ROOT_PATH__, './dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.resolve(global.__ROOT_PATH__, './dist'),
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(global.__ROOT_PATH__, './views/default.html'),
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ]
+});
