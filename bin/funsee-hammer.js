159ccd7446a07fe6fab9452fadd67a6e3f3effa2
MeatPieYan <robinyzg@hotmail.com>
2018-03-08 15:40:15 +0800

    init test

--- bin/funsee-hammer.js
+++ bin/funsee-hammer.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
+/* eslint no-unused-expressions: "off" */
+
 const yargs = require('yargs');
 const { version } = require('../package.json');
 
-const { argv } = yargs;
+// const { argv } = yargs;
 
 yargs
   .version(version)
-  .command('dev [filename]', 'run the server', (yargs) => {
-    yargs
+  .command('dev [filename]', 'run the server', (_yargs) => {
+    _yargs
       .positional('filename', {
         describe: 'dev: startup file',
         type: 'string'
@@ -19,4 +21,15 @@ yargs
   })
   .command('build', 'run the server', (argv) => {
     require('../lib/startUp/build')(argv);
-  }).argv;
+  })
+  .command('create [projectName]', 'create a project boilerplate', (_yargs) => {
+    _yargs
+      .positional('projectName', {
+        describe: 'project name, which will the dir name for your project',
+        type: 'string'
+      })
+      .version(false);
+  }, (argv) => {
+    require('../lib/boilerplate/create')(argv);
+  })
+  .argv;
