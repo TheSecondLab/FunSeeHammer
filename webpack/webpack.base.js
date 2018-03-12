14e0890a31d8ad06ccddf26190c344ccc82073eb
jiyiwohanxing <467756960@qq.com>
2018-03-12 20:07:26 +0800

    :wrench: server packaging config

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -25,7 +25,7 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['env', 'react', 'stage-0'],
+          presets: ['env', 'react'],
           plugins: [
             require('babel-plugin-add-module-exports'),
             require('babel-plugin-transform-export-extensions'),
