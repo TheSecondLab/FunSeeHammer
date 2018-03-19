70b68610b96df76e20cc2c471304d314b7bb14b8
jiyiwohanxing <467756960@qq.com>
2018-03-19 19:42:04 +0800

    :fire: set create commend independency

--- bin/funsee-hammer.js
+++ bin/funsee-hammer.js
@@ -22,14 +22,4 @@ yargs
   .command('build', 'run the server', (argv) => {
     require('../lib/startUp/build')(argv);
   })
-  .command('create [projectName]', 'create a project boilerplate', (_yargs) => {
-    _yargs
-      .positional('projectName', {
-        describe: 'project name, which will the dir name for your project',
-        type: 'string'
-      })
-      .version(false);
-  }, (argv) => {
-    require('../lib/boilerplate/create')(argv);
-  })
   .argv;
