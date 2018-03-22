3ad25dc52060a410ab10ca6d236e3bd747886076
jiyiwohanxing <467756960@qq.com>
2018-03-22 15:44:36 +0800

    :sparkles: add common saga

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -7,15 +7,22 @@ const { combineClientRouter, combineClientReducer, combineSaga } = require('../l
 // collect client router、reducer、saga config
 const clientRouters = combineClientRouter(`${global.__ROOT_PATH__}/shared/page`);
 const clientReducers = combineClientReducer(`${global.__ROOT_PATH__}/shared/page`);
+const commonReducers = combineClientReducer(`${global.__ROOT_PATH__}/shared`, 'page');
 const clientSagas = combineSaga(`${global.__ROOT_PATH__}/shared/page`);
+const commonSagas = combineSaga(`${global.__ROOT_PATH__}/shared`, 'page');
 
 global.__CLIENT_ROUTER__ = clientRouters;
 global.__CLIENT_REDUCER__ = clientReducers;
+global.__COMMON_CLIENT_REDUCER__ = commonReducers;
 global.__CLIENT_SAGA__ = clientSagas;
+global.__COMMON_CLIENT_SAGA__ = commonSagas;
 
 // trace FunSee's folder
 const sharedRelativePath = path.relative(`${global.__FS_PATH__}/lib/tool/clientRouterCreator.js`, `${global.__ROOT_PATH__}/shared/page`).replace('../', '');
 global.__RELATIVE_PATH__ = sharedRelativePath;
+
+const sharedCommonRelativePath = path.relative(`${global.__FS_PATH__}/lib/tool/clientRouterCreator.js`, `${global.__ROOT_PATH__}/shared`).replace('../', '');
+global.__COMMON_RELATIVE_PATH__ = sharedCommonRelativePath;
 
 module.exports = {
   module: {
@@ -47,9 +54,12 @@ module.exports = {
     new webpack.DefinePlugin({
       'global.__CLIENT_ROUTER__': JSON.stringify(clientRouters),
       'global.__CLIENT_REDUCER__': JSON.stringify(clientReducers),
+      'global.__COMMON_CLIENT_REDUCER__': JSON.stringify(commonReducers),
       'global.__CLIENT_SAGA__': JSON.stringify(clientSagas),
+      'global.__COMMON_CLIENT_SAGA__': JSON.stringify(commonSagas),
       'global.__ROOT_PATH__': JSON.stringify(global.__ROOT_PATH__),
       'global.__RELATIVE_PATH__': JSON.stringify(sharedRelativePath),
+      'global.__COMMON_RELATIVE_PATH__': JSON.stringify(sharedCommonRelativePath),
       __WEBPACK_REPLACE_IS_PRD__: JSON.stringify(process.env.NODE_ENV !== 'dev'),
       'global.test': JSON.stringify('webpack test')
     })
