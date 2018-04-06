39fd36be32daba20d2dd46622810376a1a59d97c
MeatPieYan <robinyzg@hotmail.com>
2018-04-06 22:52:41 +0800

    clean up logs

--- index.js
+++
@@ -1,9 +0,0 @@
-const babylon = require('babylon');
-const fs = require('fs');
-const path = require('path');
-
-const testCode = fs.readFileSync(path.resolve(__dirname, '../FunSee/index.js')).toString();
-console.log(testCode);
-const ast = babylon.parse(testCode);
-
-console.log(ast);
